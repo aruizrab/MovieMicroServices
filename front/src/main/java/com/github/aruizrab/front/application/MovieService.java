e4327ab8519cb77c50b3a8a0153aea1b8039816c
a-ruiz-rab <a.ruiz.rabasseda@gmail.com>
2021-03-24 19:42:58 +0100

    UMARAMA MIMI MA MUMU
    MI MIRI MAMA MOMO

--- front/src/main/java/com/github/aruizrab/front/application/MovieService.java
+++ front/src/main/java/com/github/aruizrab/front/application/MovieService.java
@@ -3,6 +3,7 @@ package com.github.aruizrab.front.application;
 import com.github.aruizrab.front.domain.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -20,11 +21,17 @@ public class MovieService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+
     @Value("${app.movie-service.url}")
     private String userServiceUrl;
 
     public List<Movie> getMovies(List<Long> ids) {
         var url = userServiceUrl + "/movies/{ids}";
+
+
+
         var idsToString = ids.stream().map(id -> Long.toString(id)).collect(Collectors.joining(","));
         var response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Movie>>() {
         }, idsToString);
